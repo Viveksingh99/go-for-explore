a168e9128009dd68ff39663ea503432db38873fd
Viveksingh99 <vivek22jan99@gmail.com>
2024-04-09 09:54:16 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['d2qa7a8q0vuocm.cloudfront.net'],
+      },
+};
 
 export default nextConfig;

952be53571dd3fa9124a3f91adb83f806e4d059a
Viveksingh99 <vivek22jan99@gmail.com>
2024-04-09 15:50:42 +0530

    make best trips components

---
+++ src/components/Best-Trips.tsx
@@ -0,0 +1,311 @@
+import Image from "next/image";
+import React from "react";
+
+const BestTrips = () => {
+  return (
+    <>
+      <section
+        className="trending pb-0 pt-6"
+        style={{
+          backgroundImage:
+            "url(https://d2qa7a8q0vuocm.cloudfront.net/images/shape-1.png)",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="container">
+          <div className="section-title mb-6 mx-auto text-center">
+            <h2 className="mb-1">
+              Best Trips Across <span className="theme">The Globe</span>
+            </h2>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-lg-5">
+              <a href="https://go4explore.com/trip/vietnam">
+                <div className="trend-item1 mb-4">
+                  <div className="trend-image position-relative rounded">
+                    <Image
+                      width={100}
+                      height={100}
+                      src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Vietnam.png"
+                      alt="image"
+                    />
+                    <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                      <div className="trend-content-title">
+                        <h3 className="mb-0 white">Vietnam</h3>
+                      </div>
+                    </div>
+                    <div className="color-overlay" />
+                  </div>
+                </div>
+              </a>
+              <a href="https://go4explore.com/trip/bali">
+                <div className="trend-item1 mb-4">
+                  <div className="trend-image position-relative rounded">
+                    <Image
+                      width={100}
+                      height={100}
+                      src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Bali.png"
+                      alt="image"
+                    />
+                    <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                      <div className="trend-content-title">
+                        <h3 className="mb-0 white">Bali</h3>
+                      </div>
+                    </div>
+                    <div className="color-overlay" />
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="col-lg-7">
+              <div className="row">
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/dubai">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Dubai.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Dubai</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/kashmir">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Kashmir.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Kashmir</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/kerala">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Kerala.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Kerala</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/singapore">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Singapore.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Singapore</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-6 mb-4">
+                  <a href="https://go4explore.com/trip/thailand">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Thailand.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Thailand</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/himachal-pradesh-tour-packages">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Himachal-Pradesh.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Himachal Pradesh</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/sri-lanka">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Sri-Lanka.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Sri Lanka</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/maldives">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Maldives.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Maldives</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/rajasthan">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Rajasthan.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Rajasthan</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/azerbaijan">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/azerbaijan.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Azerbaijan</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-6 mb-4">
+                  <a href="https://go4explore.com/trip/turkey">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Turkey.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Turkey</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-6 mb-4">
+                  <a href="https://go4explore.com/trip/georgia">
+                    <div className="trend-item1">
+                      <div className="trend-image position-relative rounded">
+                        <Image
+                          width={100}
+                          height={100}
+                          src="https://d2qa7a8q0vuocm.cloudfront.net/frontend/assets/img/Georgia.png"
+                          alt="image"
+                        />
+                        <div className="trend-content d-flex align-items-center justify-content-between position-absolute bottom-0 p-4 w-100 z-index">
+                          <div className="trend-content-title">
+                            <h3 className="mb-0 white">Georgia</h3>
+                          </div>
+                        </div>
+                        <div className="color-overlay" />
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BestTrips;
--- src/components/Trip-Categories.tsx
+++ src/components/Trip-Categories.tsx
@@ -1,9 +1,158 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Trip = () => {
   return (
-    <div>Trip-Categories</div>
-  )
-}
+    <>
+      <section className="categories">
+        <div className="container">
+          <div className="section-title mb-6 mx-auto text-center">
+            <h2 className="mb-1">
+              Trip <span className="theme">Categories</span>
+            </h2>
+          </div>
+          <div className="trip-categories-slider">
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/weekend-trips"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/15267120230306143345.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Weekend Trips</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/honeymoon-packages"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/11725720200405174358.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Honeymoon Packages</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/backpacking-trips"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/39798620200405174840.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Backpacking Trips</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/camping-trip"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/30756620200405174916.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Camping Trip</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/international-trips"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/1819020200405174929.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">International Trips</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/trekking"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/21029520200405174947.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Trekking</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/corporate-trips"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/13110120200405175000.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Corporate Trips</p>
+                </a>
+              </div>
+            </div>
+            <div className="col-auto" style={{ textAlign: "center" }}>
+              <div className="text-center who-are-we-steps mx-auto">
+                <a
+                  className="moveto"
+                  href="https://go4explore.com/trips-category/road-trips"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    className="w-100"
+                    src="https://d2qa7a8q0vuocm.cloudfront.net/images/29976920200405175031.png"
+                    alt="trekking"
+                  />
+                  <p className="fs-2md fw-bold mt-3">Road Trips</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Trip
+export default Trip;
